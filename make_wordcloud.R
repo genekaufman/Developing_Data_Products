d2aeaa370320d23b61f56f5fc5f8df37216e7dd9
Gene Kaufman <genekaufman@gmail.com>
2016-02-12 13:14:16 -0500

    wordcloud works, added texts

--- make_wordcloud.R
+++ make_wordcloud.R
@@ -1,30 +1,42 @@
 #http://www.r-bloggers.com/building-wordclouds-in-r/
-# i should add a timer here
+
+# alice = http://www.gutenberg.org/cache/epub/11/pg11.txt
+file.alice <- "alice.txt.gz"
+# declaration = http://www.gutenberg.org/cache/epub/1/pg1.txt
+file.declaration <- "declaration.txt.gz"
+# constitution = http://www.gutenberg.org/cache/epub/5/pg5.txt
+file.constitution <- "constitution.txt.gz"
+# mayflower = http://www.gutenberg.org/cache/epub/7/pg7.txt
+file.mayflower <- "mayflower.txt.gz"
+# hamlet = http://www.gutenberg.org/cache/epub/1787/pg1787.txt
+file.hamlet <- "hamlet.txt.gz"
+# greatexpectations = http://www.gutenberg.org/cache/epub/1400/pg1400.txt
+file.greatexpectations <- "greatexpectations.txt.gz"
+# modest = http://www.gutenberg.org/cache/epub/1080/pg1080.txt
+file.modest <- "modest.txt.gz"
+# moby = http://www.gutenberg.org/cache/epub/2701/pg2701.txt
+file.moby <- "moby.txt.gz"
+# pride = http://www.gutenberg.org/cache/epub/1342/pg1342.txt
+file.pride <- "pride.txt.gz"
 
 library(tm)
 library(SnowballC)
 library(wordcloud)
 
-jeopQ <- read.csv('JEOPARDY_CSV.csv.gz', stringsAsFactors = FALSE)
-# The actual questions are available in the Question column.
-#
-# Now, we will perform a series of operations on the text data to simplify it.
-# First, we need to create a corpus.
-jeopQ <- jeopQ[1:100,]
-jeopCorpus <- Corpus(VectorSource(jeopQ$Question))
-#jeopCorpus <- Corpus(SimpleSource(jeopQ$Question))
-vc<-VectorSource(jeopQ$Question)
+thisFile <- paste0("texts/",file.moby)
+
+thisText <- readLines(thisFile,skipNul = TRUE)
+wcCorpus <- Corpus(VectorSource(thisText))
+
 #Next, we will convert the corpus to a plain text document.
-message("step 2")
-jeopCorpus <- tm_map(jeopCorpus, PlainTextDocument)
-step2<-jeopCorpus
+wcCorpus <- tm_map(wcCorpus, PlainTextDocument)
+
 #Then, we will remove all punctuation and stopwords. Stopwords are commonly used words in the English language such as I, me, my, etc. You can see the full list of stopwords using stopwords('english').
-message("step 3")
-jeopCorpus <- tm_map(jeopCorpus, removePunctuation)
-jeopCorpus <- tm_map(jeopCorpus, removeWords, stopwords('english'))
+wcCorpus <- tm_map(wcCorpus, removePunctuation)
+wcCorpus <- tm_map(wcCorpus, removeWords, stopwords('english'))
+
 #Next, we will perform stemming. This means that all the words are converted to their stem (Ex: learning -> learn, walked -> walk, etc.). This will ensure that different forms of the word are converted to the same form and plotted only once in the wordcloud.
-message("step 4")
-jeopCorpus <- tm_map(jeopCorpus, stemDocument)
+wcCorpus <- tm_map(wcCorpus, stemDocument)
 #Now, we will plot the wordcloud.
 
-wordcloud(jeopCorpus, max.words = 100, random.order = FALSE)
+wordcloud(wcCorpus, max.words = 100, random.order = FALSE)
